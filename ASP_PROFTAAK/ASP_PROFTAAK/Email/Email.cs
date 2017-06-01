8124728fb54c015c81028d09bd974e57f419f25a
Heikum <dhrlaaboudi@gmail.com>
2017-06-01 15:42:18 +0200

    Veel gedaan. Email versturen met activatiehash done.

---
+++ ASP_PROFTAAK/ASP_PROFTAAK/Email/Email.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Web;
+
+namespace ASP_PROFTAAK.Models
+{
+    public class Email
+    {
+        public Email(string onderwerp, string ontvanger)
+        {
+            _onderwerp = onderwerp;
+            _ontvanger = ontvanger;
+        }
+
+        public Email(string onderwerp, string inhoud, string ontvanger)
+        {
+            _onderwerp = onderwerp;
+            _inhoud = inhoud;
+            _ontvanger = ontvanger;
+        }
+
+        private string _onderwerp;
+        private string _ontvanger;
+        private string _inhoud;
+
+        public string Onderwerp
+        {
+            get { return _onderwerp; }
+            set { _onderwerp = value; }
+        }
+
+        public string Inhoud
+        {
+            get { return _inhoud; }
+            set { _inhoud = value; }
+        }
+
+        public string Ontvanger
+        {
+            get { return _ontvanger; }
+            set { _ontvanger = value; }
+        }
+
+
+
+
+
+    }
+}
