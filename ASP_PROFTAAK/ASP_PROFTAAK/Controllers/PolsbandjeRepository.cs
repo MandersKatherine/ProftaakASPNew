b2c5603a3940239a956fa2e762e38147c1309ae8
Tim S <tim.sanders@student.fontys.nl>
2017-06-08 14:59:21 +0200

    Polsbandje

---
+++ ASP_PROFTAAK/ASP_PROFTAAK/Controllers/PolsbandjeRepository.cs
@@ -0,0 +1,54 @@
+﻿using ASP_PROFTAAK.App_DAL;
+using ASP_PROFTAAK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_PROFTAAK.Controllers
+{
+    public class PolsbandjeRepository
+    {
+        private readonly IPolsbandjeContext context;
+
+        public PolsbandjeRepository(IPolsbandjeContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Polsbandje> GetAllPolsbandjes()
+        {
+            return context.GetAllPolsbandjes();
+        }
+
+        public List<Polsbandje> getPolsbandjesByReserveringID(int reserveringid)
+        {
+            return context.getPolsbandjesByReserveringID(reserveringid);
+        }
+
+        public Polsbandje InsertPolsbandje(Polsbandje polsbandje)
+        {
+            return context.InsertPolsbandje(polsbandje);
+        }
+
+        public bool DeletePolsbandje(int id)
+        {
+            return context.DeletePolsbandje(id);
+        }
+
+        public bool UpdatePolsbandje(Polsbandje polsbandje)
+        {
+            return context.UpdatePolsbandje(polsbandje);
+        }
+
+        public Polsbandje GetPolsbandjeById(decimal id)
+        {
+            return context.GetPolsbandjeById(id);
+        }
+
+        public Polsbandje GetEventByPolsbandjeId(int id)
+        {
+            return context.GetEventByPolsbandjeId(id);
+        }
+    }
+}
