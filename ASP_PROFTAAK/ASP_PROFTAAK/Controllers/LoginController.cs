12c2bf24c654852613baf2be3b2357d1cc4917dd
justsander10 <sander_klijsen@hotmail.com>
2017-06-13 13:31:44 +0200

    --

--- ASP_PROFTAAK/ASP_PROFTAAK/Controllers/LoginController.cs
+++ ASP_PROFTAAK/ASP_PROFTAAK/Controllers/LoginController.cs
@@ -26,7 +26,7 @@ namespace ASP_PROFTAAK.Controllers
         [HttpPost]
         public ActionResult Login(string email, string password)
         {
-            Account loggedInUser = new Account(email, password,);
+            Account loggedInUser = new Account(email, password);
             int ID = accountrepo.GetaccountId(loggedInUser.Email);
 
             if (loginrepo.Login(email, password) == true && accountrepo.CheckActivationStatus(ID) == true)
